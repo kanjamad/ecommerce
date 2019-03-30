93db08b472ee8dbce5392ee818957b9f1f48660f
Kanjamad Bosel <npor25@yahoo.com>
2019-03-30 13:37:35 -0700

    add name and pic when clik cereal, juices and candy button

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 
-// cereal
-    // Product TitleHoney Nut Cheerios
-    // Post Honey Bunches Of Oats Cereal
-    // Kellogg's Special K Breakfast Cereal
-    // Product TitleCinnamon Toast Crunch, Breakfast Cereal
-
+// ########################   Cereal   #################################
+    
 let eat = 0;
 let photoCereal = 0;
 let cereal = [
@@ -34,7 +30,7 @@ function getCereal() {
     // console.log('get the cereal');
     for (let i = 0; i < cereal.length; i++) {
     // get all 4 boxs of  cereal
-      //  console.log(cereal[i]);
+      //  console.log(cereal[i].name);
         const eat = document.createElement('div');
         eat.innerText = cereal[i].name;
         const photoCereal = document.createElement('img');
@@ -46,27 +42,88 @@ function getCereal() {
 
 
 
+// ########################   Juices   #################################
+
+let drink = 0;
+let photoJuices = 0;
+let juices = [
+    {
+        name : "Orange Juice",
+        url : "https://images.unsplash.com/photo-1519367286790-7b682d3efdbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    },
+    {
+        name : "Melon Juice",
+        url : "https://images.unsplash.com/photo-1525385133512-2f3bdd039054?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=632&q=80",
+    },
+    {
+        name : "Strawberry cocktail drinks",
+        url : "https://images.unsplash.com/photo-1497534446932-c925b458314e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=707&q=80",
+    },
+    {
+        name : "Blueberry Cream Wine Slush",
+        url : "https://images.unsplash.com/photo-1501435544023-df4e0c503ae2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80",
+    },
+
+];
+
+const clickJuices = document.getElementById('juices');
+clickJuices.addEventListener('click', getJuices);
+
+function getJuices(){
+    console.log('get juices!!!!!!!');
+    for (let i = 0; i < juices.length ; i++){
+        // get all juices
+            // console.log(juices[i].name);
+        const drink = document.createElement('div');
+        drink.innerText = juices[i].name;
+        const photoJuices = document.createElement('img');
+        photoJuices.setAttribute('src', juices[i].url);
+        drink.appendChild(photoJuices);
+        document.querySelector('#allJuices').appendChild(drink);
+    }
+
+}
 
 
 
+// ########################   Candy   #################################
 
+let eatCandy = 0;
+let photoCandy = 0;
+let candy = [
+    {
+        name : "M&M Milk chocolate",
+        url : 'https://images.unsplash.com/photo-1534706013986-73f676db1790?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+    },
+    {
+        name : "Sour gummi worms",
+        url : 'https://images.unsplash.com/photo-1499195333224-3ce974eecb47?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=948&q=80',
+    },
+    {
+        name : "Chewy candy",
+        url : 'https://images.unsplash.com/photo-1534119139482-b530a7f9a98b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80',
+    },
+    {
+        name : "Gummi bears",
+        url : 'https://images.unsplash.com/photo-1547097465-617b04e11bb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80',
+    },
+];
 
+const clickCandy = document.getElementById('candy');
+clickCandy.addEventListener('click', getCandy);
 
-// Juices
+function getCandy() {
+    // console.log('eat all candy!!!!!!!');
+    for (let i = 0; i < candy.length ; i++){
+        // get all Candy
+            // console.log(candy[i].name);
+            const eatCandy = document.createElement('div');
+            eatCandy.innerText = candy[i].name;
+            const photoCandy = document.createElement('img');
+            photoCandy.setAttribute('src', candy[i].url);
+            eatCandy.appendChild(photoCandy);
+            document.querySelector('#allCandy').appendChild(eatCandy);
+    }
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Candy
